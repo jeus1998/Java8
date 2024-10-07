d0181b514b773bade3baac516d1ba9b067e82aec
jeus1998 <zeus20240228@gmail.com>
2024-10-07 18:58:32 +0900

    [2024:10:07] 배열 병렬 정렬

--- src/main/java/com/example/java8/App.java
+++ src/main/java/com/example/java8/App.java
@@ -1,25 +1,40 @@
 package com.example.java8;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
-@Chicken("양념")
-@Chicken("마늘간장")
-@Chicken("후라이")
+/**
+ * 배열 병렬 정렬 배열 Parallel 정렬
+ */
 public class App {
     public static void main(String[] args) throws  RuntimeException{
-        Chicken[] chickens = App.class.getDeclaredAnnotationsByType(Chicken.class);
-        Arrays.stream(chickens)
-                .forEach(c -> System.out.println(c.value()));
+        int size = 1000;
+        int [] numbers = new int [size];
+        Random random = new Random();
+        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
 
-        ChickenContainer chickenContainer = App.class.getAnnotation(ChickenContainer.class);
-        Arrays.stream(chickenContainer.value())
-                .forEach(c -> System.out.println(c.value()));
+        long start = System.nanoTime();
+        Arrays.sort(numbers); // Single Thread
+        System.out.println("serial sorting took " + (System.nanoTime() - start));
 
-    }
-    static class FeelsLikeChicken<T>{ // 타입 파라미터
-        public static <C> void print(C c){  // <C>: 타입 파라미터 C: 타입
-            System.out.println(c);
-        }
+        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+        start = System.nanoTime();
+        Arrays.parallelSort(numbers); // Multi Thread
+        System.out.println("parallel sorting took " + (System.nanoTime() - start));
     }
 }
+
+/*
+Arrays.parallelSort()
+Fork/Join 프레임워크를 사용해서 배열을 병렬로 정렬하는 기능을 제공한다.
+
+병렬 정렬 알고리즘 -> 힙 sort
+배열을 둘로 계속 쪼갠다.
+합치면서 정렬한다.
+알고리듬 효율성은 같다. 시간 O(n logN) 공간 O(n)
+
+왜 난 배열 크기가 1500이든 100000이든 그냥 정렬이 더 빠르지? CPU 코어수가 적기도 하고 컨텍스트 스위칭 오버헤드가 큰건가
+serial sorting took 40537300
+parallel sorting took 64167100
+ */
--- src/main/java/com/example/java8/ChickenContainer.java
+++
@@ -1,9 +0,0 @@
-package com.example.java8;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE_USE})
-public @interface ChickenContainer {
-    Chicken[] value();
-}
