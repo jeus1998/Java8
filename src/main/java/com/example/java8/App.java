d4b5b86c73db18a3631a01ff9c1f5e8e73b9fd9f
jeus1998 <zeus20240228@gmail.com>
2024-10-07 18:44:59 +0900

    [2024:10:07] Annotation Study2

--- src/main/java/com/example/java8/App.java
+++ src/main/java/com/example/java8/App.java
@@ -1,48 +1,25 @@
 package com.example.java8;
-import java.lang.annotation.*;
-import java.util.ArrayList;
 
-@Target(ElementType.FIELD)
-@Retention(RetentionPolicy.RUNTIME)
-@interface DateTime{
-    String yymmdd(); // 날짜
-    String hhmmss(); // 시간
-}
-enum TestType{
-    FIRST, SECOND
-}
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-@interface TestInfo{
-    int count();
-    String testedBy();
-    String[] testTools();
-    TestType testType();
-    DateTime testDate();
-}
+import java.util.Arrays;
+import java.util.List;
 
-@TestInfo(
-        count = 3,
-        testedBy = "bae",
-        testTools = {"Junit5", "AutoTester"},
-        testType = TestType.FIRST,
-        testDate = @DateTime(yymmdd = "240101", hhmmss = "235959")
-)
+@Chicken("양념")
+@Chicken("마늘간장")
+@Chicken("후라이")
 public class App {
-    public static void main(String[] args)  {
-        App app1 = new App();
-        TestInfo anno1 = app1.getClass().getAnnotation(TestInfo.class);
+    public static void main(String[] args) throws  RuntimeException{
+        Chicken[] chickens = App.class.getDeclaredAnnotationsByType(Chicken.class);
+        Arrays.stream(chickens)
+                .forEach(c -> System.out.println(c.value()));
 
-        Class<App> cls = App.class; // 메타 데이터 가져오기
-        TestInfo anno2 = cls.getAnnotation(TestInfo.class);
+        ChickenContainer chickenContainer = App.class.getAnnotation(ChickenContainer.class);
+        Arrays.stream(chickenContainer.value())
+                .forEach(c -> System.out.println(c.value()));
 
-        int count = anno1.count();
-        System.out.println("count = " + count);
-        DateTime dateTime = anno1.testDate();
-        System.out.println("dateTime = " + dateTime);
-
-        for(String tool : anno1.testTools()){
-            System.out.println("tool = " + tool);
+    }
+    static class FeelsLikeChicken<T>{ // 타입 파라미터
+        public static <C> void print(C c){  // <C>: 타입 파라미터 C: 타입
+            System.out.println(c);
         }
     }
 }
